d4064dc0e41d81985d9b2f520aee771127bf9c2a
Omkar Sharma <omkarsharma2821@gmail.com>
2023-08-12 08:12:04 +0530

    maxmin

---
+++ maxmin.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+int getmax(int num[],int size)
+{
+    int max = INT8_MIN;
+    for(int i = 0 ; i<size; i++)
+    {
+        if(num[i]>max){
+        max = num[i];
+        }
+    }
+    return max;
+}
+
+int getmin(int num[], int size)
+{
+    int min = INT8_MAX;
+    for(int i = 0 ; i<size ; i++)
+    {
+        if(num[i]<min){
+        min = num[i];
+        }
+    }
+    return min;
+}
+
+int main(){
+
+    int size;
+    int arr[100];
+
+    cout<<"Enter the size : "<<endl;
+    cin>>size;
+    cout<<"Enter the elements :"<<endl;
+    for(int i = 0 ; i<size; i++)
+    cin>>arr[i];
+
+    cout<<"maximum is "<<getmax(arr, size)<<endl;
+    cout<<"minimum is "<<getmin(arr , size)<<endl;
+
+
+
+}
