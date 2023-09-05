e5d46436929f44d389444c9b8a0e7aa538010034
Omkar Sharma <omkarsharma2821@gmail.com>
2023-09-06 03:09:12 +0530

    pair

---
+++ pair.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    pair<int, string> p;
+    p = {108 , "omkar"};
+    cout<<p.first<<" \n";
+    cout<<p.second;
+}
