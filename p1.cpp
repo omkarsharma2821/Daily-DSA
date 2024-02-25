aa98537bdd9600bbf2aaa9fae0b8db4f90f10629
Omkar Sharma <omkarsharma2821@gmail.com>
2024-02-25 12:47:28 +0530

    Revised: pattern

--- p1.cpp
+++ p1.cpp
@@ -1,13 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the value of n : "<< endl;
-    cin>>n;
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            cout<<"*";
+void printPattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
+}
+int main()
+{
+    int x;
+    cout << "Enter the value of x : " << endl;
+    cin >> x;
+    printPattern(x);
 }
