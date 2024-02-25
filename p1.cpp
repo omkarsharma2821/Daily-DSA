761e8baadb72ab432615f36a67414d18a765552f
Omkar Sharma <omkarsharma2821@gmail.com>
2024-02-25 13:33:21 +0530

    Revised: Pattern

--- p1.cpp
+++ p1.cpp
@@ -27,6 +27,14 @@ void Pattern3(int n){
         cout<<endl;
     }
 }
+void Pattern4(int n){
+    for(int i =1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     int x;
@@ -36,6 +44,6 @@ int main()
     {
         int n;
         cin >> n;
-        Pattern3(n);
+        Pattern4(n);
     }
 }
