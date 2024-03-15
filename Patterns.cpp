e7c6948954b2a1a969363d16c31a83c5c4b807ed
Omkar Sharma <omkarsharma2821@gmail.com>
2024-03-15 11:29:47 +0530

    Refactor pattern functions in Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-void Pattern1(int n){
+void Pattern1(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -10,74 +11,99 @@ void Pattern1(int n){
         cout << endl;
     }
 }
-void Pattern2(int n){
-    for(int i =0 ; i<n; i++){
-        for(int j=0; j<=i; j++){
-            cout<<"* ";
+void Pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void Pattern3(int n){
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=i; j++){
-            cout<<j<<" ";
+void Pattern3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void Pattern4(int n){
-    for(int i =1; i<=n; i++){
-        for(int j = 1; j<=i; j++){
-            cout<<i<<" ";
+void Pattern4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void Pattern5(int n){
-    for(int i =1; i<=n; i++){
-        for(int j = 0; j<n-i+1; j++){
-            cout<<"* ";
+void Pattern5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void Pattern6(int n){
-    for(int i =1; i<=n; i++){
-        for(int j = 1; j<=(n-i+1); j++){
-            cout<<j<<" ";
+void Pattern6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= (n - i + 1); j++)
+        {
+            cout << j << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
-void Pattern7(int n){
-    for(int i = 0; i<n; i++){
+void Pattern7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         // space
-        for(int j = 0; j<n-i-1; j++){
-            cout<<" ";
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
         }
-        //start
-            for(int j = 0; j<2*i+1; j++){
-                cout<<"*";
-            }
-            for(int j = 0; j<n-i-1; j++){
-            cout<<" ";
+        // start
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
         }
-        cout<<endl;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
     }
 }
-void Pattern8(int n){
-    for(int i = 0; i<n; i++){
-        for(int j =0; j<=i;j++){
-            cout<<" ";
+void Pattern8(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " ";
         }
-        for(int j = 0; j<2*n-(2*i+1); j++){
-            cout<<"*";
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
         }
-        for(int j =0; j<=i;j++){
-            cout<<" ";
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 int main()
@@ -89,6 +115,6 @@ int main()
     {
         int n;
         cin >> n;
-        Pattern7(n);
+        Pattern2(n);
     }
 }
