245c80befb216ba6eb324217e646a6343f9ff6c0
Omkar Sharma <omkarsharma2821@gmail.com>
2024-03-20 18:03:54 +0530

    Refactor Node class constructor in Linked_List.cpp

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -6,14 +6,20 @@ class Node{
     int data;
     Node* next;
 
+    public:
     Node(int data1, Node* next1){
         data = data1;
         next = next1;
     }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
 };
 int main() {
     vector<int> arr = {2,3,5,4,7,5};
-    Node* y = new Node(arr[5], nullptr);
+    Node* y = new Node(arr[5]);
     cout<<y->data<<endl;
     cout<<y->next;
 }
