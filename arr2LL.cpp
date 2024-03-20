ced142ce070cc69da4526341c498ac50c58cbee5
Omkar Sharma <omkarsharma2821@gmail.com>
2024-03-20 18:28:58 +0530

    Update user authentication logic

---
+++ arr2LL.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* ConvertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+int main() {
+    vector<int> arr = {200,3,5,4,7,5};
+    Node* head = ConvertArr2LL(arr);
+    cout<<head->data<<endl;
+}
