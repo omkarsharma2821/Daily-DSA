ad854e95fdad099734a78311aed7f08b4a9710fe
Omkar Sharma <omkarsharma2821@gmail.com>
2024-03-29 23:58:10 +0530

    Refactored rotate function to accept additional parameters

--- rotate.cpp
+++ rotate.cpp
@@ -1,14 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void rotate(vector<int>& arr, int d){
-    int n = arr.size();
+void rotate(vector<int>& arr, int n, int d){
     d = d%n;
     reverse(arr.begin(),arr.begin()+d);
     reverse(arr.begin()+d,arr.end());
     reverse(arr.begin(),arr.end());
 }
-void print(vector<int> &arr, int size){
+void print(vector<int> &arr, int size, int d){
     for(int i = 0; i<size; i++)
     {
         cout<<arr[i]<<" ";
@@ -16,7 +15,7 @@ void print(vector<int> &arr, int size){
 }
 int main() {
     vector<int> arr = {1,2,3,4,5};
-    rotate(arr, 5);
-    print(arr, 5);
+    rotate(arr,5, 1);
+    print(arr,5, 1);
     return 0;
 }
