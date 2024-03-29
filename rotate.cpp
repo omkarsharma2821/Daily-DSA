8c9b9c7496ed97a827d80fde693243f23cde0de5
Omkar Sharma <omkarsharma2821@gmail.com>
2024-03-29 23:55:20 +0530

    Fix rotate function and add print function

--- rotate.cpp
+++ rotate.cpp
@@ -1,16 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int rotate(vector<int>&arr, int d){
+void rotate(vector<int>& arr, int d){
     int n = arr.size();
     d = d%n;
     reverse(arr.begin(),arr.begin()+d);
     reverse(arr.begin()+d,arr.end());
     reverse(arr.begin(),arr.end());
-    return arr;
+}
+void print(vector<int> &arr, int size){
+    for(int i = 0; i<size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
 int main() {
-    int arr[5] = {1,2,3,4,5};
+    vector<int> arr = {1,2,3,4,5};
     rotate(arr, 5);
+    print(arr, 5);
     return 0;
 }
