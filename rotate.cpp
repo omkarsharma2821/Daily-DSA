3b197970b2ea6ec93281db5c88b25052dd12e0f9
Omkar Sharma <omkarsharma2821@gmail.com>
2024-03-29 23:47:07 +0530

    Add rotate function to reverse elements in an array

---
+++ rotate.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rotate(vector<int>&arr, int d){
+    int n = arr.size();
+    d = d%n;
+    reverse(arr.begin(),arr.begin()+d);
+    reverse(arr.begin()+d,arr.end());
+    reverse(arr.begin(),arr.end());
+    return arr;
+}
+int main() {
+    
+    return 0;
+}
