ae2d6d3f8037b4a3dd8f0be7633968442d9abed7
Omkar Sharma <omkarsharma2821@gmail.com>
2024-04-11 23:36:28 +0530

    rotate arr

--- rotate.cpp
+++ rotate.cpp
@@ -1,21 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void rotate(vector<int>& arr, int n, int d){
-    d = d%n;
-    reverse(arr.begin(),arr.begin()+d);
-    reverse(arr.begin()+d,arr.end());
-    reverse(arr.begin(),arr.end());
+void rotate(vector<int> &arr, int n, int d)
+{
+    d = d % n;
+    reverse(arr.begin(), arr.begin() + d);
+    reverse(arr.begin() + d, arr.end());
+    reverse(arr.begin(), arr.end());
 }
-void print(vector<int> &arr, int size, int d){
-    for(int i = 0; i<size; i++)
+void print(vector<int> &arr, int size, int d)
+{
+    for (int i = 0; i < size; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
 }
-int main() {
-    vector<int> arr = {1,2,3,4,5};
-    rotate(arr,5, 1);
-    print(arr,5, 1);
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    rotate(arr, 5, 1);
+    print(arr, 5, 1);
     return 0;
 }
