d300e1f788e7af7121e9f5cda0f6deeeca4c2cae
Omkar Sharma <omkarsharma2821@gmail.com>
2024-04-13 23:46:33 +0530

    reverse

--- reverse.cpp
+++ reverse.cpp
@@ -1,34 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int reverse(int arr[], int size)
-{
+int reverse(int arr[], int size){
     int start = 0;
-    int end = size - 1;
-    while (start <= end)
-    {
-        swap(arr[start], arr[end]);
+    int end = size-1;
+    while(start<=end){
+        swap(arr[start] , arr[end]);
         start++;
         end--;
     }
     return 0;
 }
 
-void printarray(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
+void printarray(int arr[], int size){
+    for(int i = 0; i<size; i++)
     {
-        cout << arr[i] << " ";
+        cout<<arr[i]<<" ";
     }
 }
 
-int main()
-{
+int main(){
 
     int i;
     int n;
-    int arr[20] = {1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 28};
-    reverse(arr, 20);
-    printarray(arr, 20);
-    return 0;
+    int arr[20] = {1,2,3,4,5,0,0,0,0,0,0,0,28};
+        reverse(arr, 20);
+        printarray(arr, 20);
+        return 0;
 }
