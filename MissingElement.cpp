3d4aee2dadaa15beee924366492ae22ca6725e30
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-11 18:07:38 +0530

    Missing Element

---
+++ MissingElement.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Missing_Element(int arr[], int n){
+    int sum = 0;
+    int sum1 = n*(n+1)/2;
+    for(int i = 0; i<n-1; i++){
+        sum += arr[i]; 
+    }
+    return sum1-sum;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n-1; i++){
+        cin>>arr[i];
+    }
+    int ans = Missing_Element(arr, n);
+    cout<<ans;
+}
