13e3861fbba70693ba20be5e52cead882ba9b151
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-15 23:53:19 +0530

    MoveZerosToEnd.cpp

--- MoveZerosToEnd.cpp
+++ MoveZerosToEnd.cpp
@@ -1,27 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
-void MoveZeroToEnd(vector<int> &arr, int n){
+void MoveZeroToEnd(vector<int> &arr, int n)
+{
     int j = -1;
-    for(int i = 0; i<n; i++){
-        if(arr[i] == 0){
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 0)
+        {
             j = i;
             break;
         }
     }
-    for(int i = j+1; i<n; i++){
-        if(arr[i] != 0 && j != -1){
+    for (int i = j + 1; i < n; i++)
+    {
+        if (arr[i] != 0 && j != -1)
+        {
             swap(arr[i], arr[j]);
             j++;
         }
     }
 }
 
-int main(){
-    vector<int> arr = {1,0,2,2,1,0,1,2,5,0,6,7,8,9};
+int main()
+{
+    vector<int> arr = {1, 0, 2, 2, 1, 0, 1, 2, 5, 0, 6, 7, 8, 9};
     int n = arr.size();
-    MoveZeroToEnd(arr,n);
-    for(int i = 0; i<n; i++){
-        cout<<arr[i]<<" ";
+    MoveZeroToEnd(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
