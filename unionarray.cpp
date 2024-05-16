812b964d9fe0127178f6299da64d8db4d6a297c0
Omkar Sharma <omkarsharma2821@gmail.com>
2024-05-16 23:38:57 +0530

    FindUnion

--- unionarray.cpp
+++ unionarray.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
-  int i = 0, j = 0; // pointers
-  vector < int > Union; // Uninon vector
+  int i = 0, j = 0; 
+  vector < int > Union; 
   while (i < n && j < m) {
     if (arr1[i] <= arr2[j]) // Case 1 and 2
     {
