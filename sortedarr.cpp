9de80093fbd5621c1f30a26ad947cd3a105dc758
Omkar Sharma <omkarsharma2821@gmail.com>
2024-07-07 23:53:50 +0530

    sortedorder

--- sortedarr.cpp
+++ sortedarr.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 bool Check_Sorted_Array(vector<int>arr, int n){
     for(int i = 1; i<n; i++){
         if(arr[i]<arr[i-1])
@@ -8,7 +7,6 @@ bool Check_Sorted_Array(vector<int>arr, int n){
     }
     return true;
 }
-
 int main(){
     vector<int>arr = {1,2,3,4,5,6,7,8,9,10};
     int n = arr.size();
