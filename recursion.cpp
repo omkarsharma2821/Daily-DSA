80b91695239a190a0db1c144e44bc1bc7ce74b58
Omkar Sharma <omkarsharma2821@gmail.com>
2024-08-25 15:00:33 +0530

    recursion practice

--- recursion.cpp
+++ recursion.cpp
@@ -2,6 +2,7 @@
 // stackoverflow and segmentation error
 
 // print name N times using recursion 
+// print N to  1 
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -11,9 +12,15 @@ void print(int i, int n){
     cout<<"Omkar 💀"<<endl;
     print(i+1, n);
 }
+void printNto1(int i, int n){
+    if(i<1) return;
+    cout<<i<<endl;
+    printNto1(i-1, n);
+}
 
 int main(){
     int n;
     cin>>n;
     print(1,n);
+    printNto1(n, n);
 }
